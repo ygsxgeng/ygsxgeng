c5e82f461c6d116e80623f7026d5355deb4d1b98
殷智勇 <58811458+q718424733@users.noreply.github.com>
2020-04-10 20:49:59 +0800

    4.10
    完成商品列表大体和详情页面部分

--- src/views/Productlist.vue
+++ src/views/Productlist.vue
@@ -1,47 +1,96 @@
 <template>
     <div class="productlist">
-        productlist
+        <ul>
+            <li 
+                v-for="item in productlist"
+                :key="item.CommodityId"
+                tag="li"
+            >       
+            <van-icon class="in" name="plus" />
+                <van-card
+                    @click="hanldPage(item.CommodityCode)"
+                    :price="item.OriginalPrice"
+                    :desc="item.Spec"
+                    :title="item.CommodityName"
+                    :thumb="item.SmallPic"
+                    class="list"
+                >
+                </van-card>
+            </li>
+        </ul>
+        
+
     </div>
 </template>
 <script>
-import axios from "axios"
+import Vue from 'vue';
+import { instance } from "@/utils/http"
+import { Card,tag,Icon } from 'vant';
+Vue.use(Card).use(Icon);
 export default {
+    data() {
+        return {
+            productlist:[]
+        }
+    },
     props:["id"],
     methods:{
-        axiosList({data1,data2=4}){
-            axios({
-                url:"https://b2capigateway.yiguo.com/api/commodityapi/Commodity/GetSearchList?appName=3000025",
-                method:"post",
-                data:{
-                    Body:{
-                        CategoryCode: "",
-                        CategoryId: data1,
-                        Keyword: "",
-                        PageIndex: 1,
-                        Sort: data2
-                    },
-                    Head:{
-                        CityCode: "128",
-                        CityId: "a10524be-acea-451e-9280-df0514e11e2b",
-                        DeviceId: "0951101603da996cca55b346b0a39efd",
-                        DistrictId: "c9e2297f-a036-47bd-b822-2a946e4162d0",
-                        LoginToken: "",
-                        MobileOS: "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
-                        Token: ""
+        hanldPage(data){
+        this.$router.push({
+                    name:"detail",
+                    params:{
+                        id:data
                     }
-                }
-            }).then(res=>{
-                console.log(res)
-            })
+                })
         }
     },
     created(){
-        console.log(this.id)
-        this.axiosList({data1:this.id})
+        instance.post("/api/commodityapi/Commodity/GetSearchList?appName=3000025",{
+            ...{
+                    Head:{
+                        CityCode: "1",
+                        CityId: "312d0556-0671-4f2e-8bac-7b8873b5a03a",
+                        DistrictId: "ab1cfb9d-e721-4861-9415-ba8940e8c087",
+                        DeviceId: "9fc12613b859c8899b96c360373e76d4",
+                        LoginToken: "",
+                        Token: ""
+                    },
+                    Body:{
+                            CategoryCode: "",
+                            CategoryId: this.$route.params.id,
+                            Keyword: "",
+                            PageIndex: 1,
+                            Sort: 4
+                        }
+                }
+        }).then(res=>{
+                this.productlist  = res.data.Data.CommodityList
+            })
+    },
+    mounted(){
+        
     }
+    
 }
 </script>
 
-<style lang="sass" scoped>
 
+<style lang="scss" scoped>
+    .list{
+        background:#fff;
+        img{
+            width: 100%;
+            height: 100%;
+        }
+    }
+    li{ 
+        position: relative;
+        .in{
+        position:absolute;
+        top: 0.5rem;
+        right: 0.2rem;
+        z-index: 10;
+        font-size: 0.2rem;;
+        }
+    }
 </style>
